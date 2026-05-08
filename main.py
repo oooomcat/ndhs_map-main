0ef64192e3eb1a083da905a7cd9addb8d9c8714e
oooomcat <kko77-s250321@gclass.ice.go.kr>
2026-05-08 16:15:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ m = folium.Map(
 )
 folium.Marker(
       location=[37.404160, 126.719249],
-      popup = "클릭해보세요
+      popup = "클릭해보세요"
       tooltip= "남동고등학교", 
       icon = folium.Icon(color='purple', icon='info-sign')
   ).add_to(m)
