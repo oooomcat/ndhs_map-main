bc8180751ff1700392529b42da3164f101b8e562
oooomcat <kko77-s250321@gclass.ice.go.kr>
2026-05-08 16:15:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,21 @@ st.text("안녕^-^")
 
 st.code("a=3")
 
+#데이터 읽어오기(데이터 수집 csv)
 df = pd.read_csv('인천광역시 남동구_고등학교_20240325.csv', encoding = 'cp949')
 df_latlon = df[['위도','경도']]
 df_latlon = df_latlon.rename(columns={'위도':'lat','경도':'lon'})
 st.map(df_latlon)
+
+# 지도 생성 및 마커 표시(시각화 단계)
+m = folium.Map(
+    location=[
+37.404160, 126.719249],
+    zoom_start=12
+)
+folium.Marker(
+      location=[37.404160, 126.719249],
+      popup = "클릭해보세요
+      tooltip= "남동고등학교", 
+      icon = folium.Icon(color='purple', icon='info-sign')
+  ).add_to(m)
