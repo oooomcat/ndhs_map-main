44de014e5e1ac8c7f6126df3dca9b4dbe9944d96
oooomcat <kko77-s250321@gclass.ice.go.kr>
2026-05-08 15:46:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # main.py
 import streamlit as st
 import folium
-from strimlit_folium import st_folium
+from streamlit_folium import st_folium
 import pandas as pd
 
 # 1. 웹 페이지 설정
 st.set_page_config(page_title="남동고 등산 메이트", layout="width")
+
+st.title("2026 학교 등산 행사 안내 지도")
 st.text("안녕^-^")
