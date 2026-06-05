93a4497584c890493aedf4c05a717b2275bdc085
oooomcat <kko77-s250321@gclass.ice.go.kr>
2026-06-05 15:55:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ st.code("a=3")
 
 #데이터 읽어오기(데이터 수집 csv)
 # df = pd.read_csv('인천광역시 남동구_고등학교_20240325.csv', encoding = 'utf-8')
-df = pd.read_csv('등산경로.csv', encoding = 'cp949')
+df = pd.read_csv('등산경로.csv', encoding = 'utf-8')
 df_latlon = df[['위도','경도']]
 df_latlon = df_latlon.rename(columns={'위도':'lat','경도':'lon'})
 # st.map(df_latlon)
