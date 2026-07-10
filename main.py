46f55db1a229e28aa6e6165450f1c6249889d885
oooomcat <kko77-s250321@gclass.ice.go.kr>
2026-07-10 16:10:30 +0900

    Renumbered sections in main.py for clarity
    Updated section headers for better clarity and organization.

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ st.text("안녕^-^")
 
 st.code("a=3")
 
-#데이터 읽어오기(데이터 수집 csv)
+# 2. 데이터 읽어오기(데이터 수집 csv)
 # df = pd.read_csv('인천광역시 남동구_고등학교_20240325.csv', encoding = 'utf-8')
 df = pd.read_csv('등산경로.csv', encoding = 'utf-8')
 df_latlon = df[['위도','경도']]
 df_latlon = df_latlon.rename(columns={'위도':'lat','경도':'lon'})
 # st.map(df_latlon)
 
-# 지도 생성 및 마커 표시(시각화 단계)
+# 3. 지도 생성 및 마커 표시(시각화 단계)
 m = folium.Map(
     location=[
 37.405969, 126.721529],
@@ -44,5 +44,13 @@ for i in range(len(df)):
         icon = folium.Icon(color='purple', icon='info-sign')
     ).add_to(m)
 
-#화면 출력
+# 4.화면 출력
+col1, col2 = st.columns([3,1])
+with col1:
+    st_folium(m, width=700, height=500)
+with col2:
+    st.subheader("정보") #코스정보
+    st.info("길이 미끄럽습니다. 주의하세요.")
+    st.metric(label="소요시간", value="10분")
+    st.write("주의사항 : 등산화를 착용하세요.(●'◡'●)")
 st_folium(m, width=700, height=500)
