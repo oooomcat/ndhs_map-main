7fb177f9024d456aae46b745115315f2c2585767
oooomcat <kko77-s250321@gclass.ice.go.kr>
2026-08-21 15:47:43 +0900

    Add os module import to main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import streamlit as st
 import folium
 from streamlit_folium import st_folium
 import pandas as pd
+import os
 
 # 1. 웹 페이지 설정
 st.set_page_config(page_title="남동고 등산 메이트", layout="wide")
